4c05caac4424d074af80e7d101baaed5ceef621f
Miguel Avendaño (MAW) <43830968+mawnowgithub@users.noreply.github.com>
2024-02-07 16:26:36 -0500

    first commit
    hana's invitation files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const ready = () => {
+	console.log("ready");
+
+	const petalButton = document.querySelector("#petal");
+	const fallingPetals = document.querySelector("#falling");
+	const scene1 = document.querySelector(".scene-1");
+	const scene2 = document.querySelector(".scene-2");
+	petalButton.addEventListener("click", () => {
+		fallingPetals.style.visibility = "visible";
+		scene1.classList.add("animating");
+		petalButton.classList.add("active");
+		const playScene2 = setTimeout(() => {
+			scene2.classList.add("animating");
+			clearTimeout(playScene2);
+		}, 800);
+	});
+};
+
+window.addEventListener("DOMContentLoaded", ready);
