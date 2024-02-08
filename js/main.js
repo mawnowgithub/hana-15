aeea540b9c3403057989495bcfb52ec0f95df11b
mawnowgithub <mawcontacto@gmail.com>
2024-02-07 22:55:56 -0500

    forth commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const ready = () => {
-	console.log("ready");
-
 	const petalButton = document.querySelector("#petal");
 	const fallingPetals = document.querySelector("#falling");
 	const scene1 = document.querySelector(".scene-1");
 	const scene2 = document.querySelector(".scene-2");
+	const infoButton = document.querySelector(".scene-2__info");
+	const modal = document.querySelector(".modal");
+	const modalButton = document.querySelector(".modal__button");
+
 	petalButton.addEventListener("click", () => {
 		fallingPetals.style.visibility = "visible";
 		scene1.classList.add("animating");
@@ -13,6 +15,21 @@ const ready = () => {
 			scene2.classList.add("animating");
 			clearTimeout(playScene2);
 		}, 1500);
+		const animateInfo = setTimeout(() => {
+			infoButton.classList.add("active");
+			clearTimeout(animateInfo);
+			scene2.classList.add("active");
+		}, 2500);
+	});
+
+	infoButton.addEventListener("click", () => {
+		console.log("click");
+		modal.classList.add("active");
+		infoButton.classList.add("pressed");
+	});
+
+	modalButton.addEventListener("click", () => {
+		modal.classList.remove("active");
 	});
 };
 
