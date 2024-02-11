34d890363ee3fbe3128ba1d7f754717ca7d9c178
mawnowgithub <mawcontacto@gmail.com>
2024-02-11 12:23:48 -0500

    This is a brand new commit. Enjoy it!

--- js/main.js
+++ js/main.js
@@ -31,8 +31,8 @@ const ready = () => {
 		let name = urlParams.get("name");
 		const genre = urlParams.get("gen");
 		name = name?.replaceAll("_", " ");
-		const plural = name?.search(" y ") > -1 || name?.search("F.") > -1;
-
+		const plural = name?.search(" y ") > -1 || name?.search("F. ") > -1;
+		console.log(plural);
 		if (name) {
 			frontGreet.innerHTML = name;
 			modalGreetTitle.innerHTML = name;
